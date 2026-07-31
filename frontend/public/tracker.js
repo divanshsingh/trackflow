02b87008420132417816d8dcc968017e972e34b7
Divansh <divanshsingh1612@gmail.com>
2026-07-31 11:48:42 +0530

    Configure production API endpoint for tracker

---
+++ frontend/public/tracker.js
@@ -0,0 +1,38 @@
+(function(){
+    const script = document.currentScript;
+    const apiKey = script.dataset.apiKey;
+    const endpoint = script.dataset.endpoint || "https://trackflow-vaps.onrender.com";
+
+    function trackPage(){
+    fetch(`${endpoint}/api/track`, {
+        method: "POST",
+        credentials: "include", // very important (cookies)
+        headers: {
+            "Content-Type": "application/json",
+        }, 
+        body: JSON.stringify({
+            apiKey,
+            path: window.location.pathname,
+            title: document.title,
+            referrer: document.referrer,
+        })       
+    })
+    }
+    trackPage();
+
+    const originalPushState = history.pushState;
+    const originalReplaceState = history.replaceState;
+
+    history.pushState = function(...args){
+        originalPushState.apply(history, args);
+        trackPage();
+    }
+
+    history.replaceState = function(...args){
+        originalReplaceState.apply(history, args);
+        trackPage();
+    }        
+    window.addEventListener("popstate", () => {
+        trackPage();
+    });
+})();
