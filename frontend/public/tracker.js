19cd0937af1f8013003b02a058639315375831fa
Divansh <divanshsingh1612@gmail.com>
2026-08-02 08:23:57 +0530

    Change storage of visitorId and sessionId from cookies to localStorage

--- frontend/public/tracker.js
+++ frontend/public/tracker.js
@@ -3,19 +3,35 @@
     const apiKey = script.dataset.apiKey;
     const endpoint = script.dataset.endpoint || "https://trackflow-vaps.onrender.com";
 
-    function trackPage(){
-    fetch(`${endpoint}/api/track`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        }, 
-        body: JSON.stringify({
-            apiKey,
-            path: window.location.pathname,
-            title: document.title,
-            referrer: document.referrer,
-        })       
-    })
+    async function trackPage(){
+        let visitorId = localStorage.getItem("trackflow_visitor_id");
+        let sessionId = sessionStorage.getItem("trackflow_session_id");
+        const res = await fetch(`${endpoint}/api/track`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            }, 
+            body: JSON.stringify({
+                apiKey,
+                visitorId,
+                sessionId,
+                path: window.location.pathname,
+                title: document.title,
+                referrer: document.referrer,
+            })       
+        })
+        const data = await res.json();
+        if (data.success) {
+            localStorage.setItem(
+                    "trackflow_visitor_id",
+                    data.data.visitorId
+            );
+            
+            sessionStorage.setItem(
+                    "trackflow_session_id",
+                    data.data.sessionId
+            );
+        }
     }
     trackPage();
 
